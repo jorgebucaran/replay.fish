b0879513dc2a3eaacf126929f48502ed037f5c9d
Jorge Bucaran <mail@jorgebucaran.com>
2020-12-08 15:24:38 +0900

    Newlines everywhere

--- completions/replay.fish
+++ completions/replay.fish
@@ -1,4 +1,3 @@
 complete --command replay --no-files
 complete --command replay --exclusive --long version -d "Print version"
 complete --command replay --exclusive --long help -d "Print this help message"
-
