46a170a25c6e2ffa09d79f890d998ccbe6f39c06
Jorge Bucaran <mail@jorgebucaran.com>
2020-12-28 22:01:20 +0900

    EOF newlines

--- functions/bax.fish
+++ functions/bax.fish
@@ -1,4 +1,4 @@
 function bax
-  echo (set_color red --bold)"Warning: `bax` is now `replay`. Please use `replay` to supress this warning."(set_color normal)
-  replay $argv
+    echo (set_color red --bold)"Warning: `bax` is now `replay`. Please use `replay` to supress this warning."(set_color normal)
+    replay $argv
 end
