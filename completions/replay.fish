9b9cbc6a07faada7ec66145eb6773ee882752608
Jorge Bucaran <mail@jorgebucaran.com>
2021-01-03 04:05:55 +0900

    Verbose

--- completions/replay.fish
+++ completions/replay.fish
@@ -1,3 +1,3 @@
 complete --command replay --no-files
-complete --command replay --exclusive --long version -d "Print version"
-complete --command replay --exclusive --long help -d "Print this help message"
+complete --command replay --exclusive --long version --description "Print version"
+complete --command replay --exclusive --long help --description "Print this help message"
